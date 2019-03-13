b3a8ea8b13ba995ef0319c202cd8fc152f33e724
vue-go.js <38337842+15234894457@users.noreply.github.com>
2019-03-13 23:48:49 +0800

    Update Fanout.java

--- src/main/java/com/neo/web/Fanout.java
+++ src/main/java/com/neo/web/Fanout.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
 
 import com.neo.rabbit.fanout.FanoutSender;
 
