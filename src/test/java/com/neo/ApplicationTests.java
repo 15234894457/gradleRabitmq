e95dce2ddefd52a0ae48658919cac81ac5948b7f
vue-go.js <38337842+15234894457@users.noreply.github.com>
2019-04-17 15:17:10 +0800

    Update ApplicationTests.java

--- src/test/java/com/neo/ApplicationTests.java
+++ src/test/java/com/neo/ApplicationTests.java
@@ -15,3 +15,73 @@
 //	}
 //
 //}
+/*import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.http.MediaType;
+
+/**
+ * 环境测试
+ */
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class EnvironmentTest {
+	
+	/**
+     * 模拟mvc测试对象
+     */
+    private MockMvc mockMvc;
+
+    /**
+     * web项目上下文
+     */
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    /**
+     * 业务数据接口
+     */
+	@Autowired
+	//private SupplierService supplierService;
+
+    /**
+     * 所有测试方法执行之前执行该方法
+     */
+    @Before
+    public void before() {
+        //获取mockmvc对象实例
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    /**
+     * 测试访问/index地址
+     * @throws Exception
+     */
+    @Test
+    public void testQueryUniversityByStudyType(){
+        String url = String.format("/userresou/findResou");
+        MvcResult result = null;
+		try {
+			result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON))
+			        .andDo(print())
+			        .andReturn();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Assert.assertEquals(200, result.getResponse().getStatus());
+    }
+}*/
